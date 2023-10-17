cbe0973076fe262ba1cd66c89373f7b1ade15c02
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-17 21:40:04 +0000

    Library

--- monty.h
+++ monty.h
@@ -13,10 +13,10 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-/* Function prototypes for task 0 */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
-/* Additional utility functions can be declared here */
+int is_integer(const char *str);
+int push_stack(stack_t **stack, int value);
 
 #endif /* MONTY_H */
