bfbd52ccae01390600cb44a05b2251dbcc1ebf44
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-19 13:12:15 +0000

    push pall

--- push_pall.c
+++ push_pall.c
@@ -1,12 +1,23 @@
 #include "monty.h"
 
-int push_stack(stack_t **stack, int value)
+/**
+ * push - Pushes an element onto the stack
+ * @stack: A pointer to the stack
+ * @value: The integer value to be pushed onto the stack
+ *
+ * Description: This function allocates memory for a new node, assigns the
+ * provided integer value to it, and adds it to the stack.
+ *
+ * Return: None
+ */
+void push(stack_t **stack, int value)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
 
 	if (!new_node)
 	{
-		return (0);
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
 	}
 
 	new_node->n = value;
@@ -14,44 +25,23 @@ int push_stack(stack_t **stack, int value)
 	new_node->next = *stack;
 
 	if (*stack)
-	{
 		(*stack)->prev = new_node;
-	}
-
 	*stack = new_node;
-	return (1);
 }
 
-int is_integer(const char *str)
-{
-	(void)str;
-	return (0);
-}
-
-void push(stack_t **stack, unsigned int line_number)
-{
-	char *arg = strtok(NULL, " \t\n");
-	int n;
-
-	if (!arg || !is_integer(arg))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	n = atoi(arg);
-	if (!push_stack(stack, n))
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-}
-
+/**
+ * pall - Prints all values on the stack
+ * @stack: A pointer to the stack
+ * @line_number: The current line number in the input file
+ *
+ * Description: This function prints all the values in the stack from top to
+ * bottom. If the stack is empty, it doesn't print anything.
+ *
+ * Return: None
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
-
-	(void)line_number;
 
 	while (current)
 	{
